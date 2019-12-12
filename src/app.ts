76d70a36584bc83aa6455d3527b71b1386188aa2
Andrey <aa.lubochnikov@gmail.com>
2019-12-12 23:31:16 +0300

    mapped-commit

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,37 @@
-class Pizza {
-    public toppings: string[] = [];
-    constructor (readonly name:string) {}
-    public addTopping(topping: string){
-        this.toppings.push(topping)
-    }
+// const person = {
+//     name: 'Todd',
+//     age: 27
+// }
+
+// type Person = typeof person;
+// type PersonKeys = keyof Person;
+// type PersonTypes = Person[PersonKeys];
+
+// function getProperty<T, K extends keyof T>(obj: T, key:K){
+//     return obj[key];
+// }
+
+// const personName = getProperty(person, 'age')
+
+type MyPartial<T> = {
+    [P in keyof T]?: T[P]
 }
 
-const pizza = new Pizza('Pepperoni')
-pizza.addTopping('pepperoni')
+interface Person {
+    name: string;
+    age: number;
+}
+
+interface PartialPerson {
+    name?: string;
+    age?: number;
+}
+
+function updatePerson(person: Person, prop: MyPartial<Person>){
+    return { ...person, ...prop}
+}
+
+const person: MyPartial<Person> = {
+    name: 'Todd',
+}
+
